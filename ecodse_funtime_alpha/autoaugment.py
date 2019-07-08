e706665557b5e4f161eebb209669d78202b46205
Simon Blackburn <simon.blackburn@mila.quebec>
2019-07-08 16:49:29 -0400

    ENH: auto-augment CIFAR10 policy

---
+++ ecodse_funtime_alpha/autoaugment.py
@@ -0,0 +1,392 @@
+import numpy as np
+import PIL
+import tensorflow as tf
+
+from PIL import ImageOps
+
+
+def pil_wrap(img):
+    """Convert the img tensor to a PIL Image
+
+    Parameters
+    ----------
+    img : tf.tensor
+        image to wrap to PIL
+
+    Returns
+    -------
+    PIL image
+        image in PIL format
+    """
+    return PIL.Image.fromarray(np.uint8(img.numpy() * 255))
+
+
+def pil_unwrap(pil_img):
+    """Convert the 'pil_img' image to a tensorflow tensor
+
+    Parameters
+    ----------
+    pil_img : PIL image
+        image to convert
+
+    Returns
+    -------
+    tf.tensor
+        image as a tensorflow tensor
+    """
+    pic_array = (np.array(pil_img.getdata()).reshape((256, 256, 3)) / 255)
+    return tf.convert_to_tensor(pic_array)
+
+
+def autocontrast(img, magnitude=None):
+    """apply autocontrast to image
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int, optional
+        amplitude of the transformation, unused here, by default: None
+
+    Returns
+    -------
+    tf.tensor
+        image enhanced with autocontrast
+    """
+    return pil_unwrap(ImageOps.autocontrast(pil_wrap(img)))
+
+
+def brightness(img, magnitude):
+    """enhance brightness
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image as tensorflow tensor
+    magnitude : int
+        brightness magnitude, between 0 and 10
+
+    Returns
+    -------
+    tf.tensor
+        transformed image
+    """
+    magnitude = magnitude * 1.8 / 10 + 0.1
+    return pil_unwrap(PIL.ImageEnhance.Brightness(pil_wrap(img)).enhance(magnitude))
+
+
+def color(img, magnitude):
+    """enhance colors
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image as tensorflow tensor
+    magnitude : int
+        color change magnitude, between 0 and 10
+
+    Returns
+    -------
+    tf.tensor
+        transformed image
+    """
+    magnitude = magnitude * 1.8 / 10 + 0.1
+    return pil_unwrap(PIL.ImageEnhance.Color(pil_wrap(img)).enhance(magnitude))
+
+
+def contrast(img, magnitude):
+    """increase image contrast
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int
+        constrast magnitude between 0 and 10
+
+    Returns
+    -------
+    tf.tensor
+        transformed image
+    """
+    # rescale magnitude
+    magnitude = magnitude * 1.8 / 10 + 0.1
+    return pil_unwrap(PIL.ImageEnhance.Contrast(pil_wrap(img)).enhance(magnitude))
+
+
+def equalize(img, magnitude=None):
+    """equalize image
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int, optional
+        amplitude of the transformation, unused here, by default: None
+
+    Returns
+    -------
+    tf.tensor
+        equalized image
+    """
+    return pil_unwrap(ImageOps.equalize(pil_wrap(img)))
+
+
+def invert(img, magnitude=None):
+    """inverts colors
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int, optional
+        amplitude of the transformation, unused here, by default: None
+
+    Returns
+    -------
+    tf.tensor
+        transformed image
+    """
+    return pil_unwrap(ImageOps.invert(pil_wrap(img)))
+
+
+def posterize(img, magnitude):
+    """posterize image
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int
+        level of posterize between 0 and 10
+
+    Returns
+    -------
+    tf.tensor
+        posterized image as tensorflow tensor
+    """
+    magnitude = int(magnitude * 4 / 10)
+    return pil_unwrap(ImageOps.posterize(pil_wrap(img), 4 - magnitude))
+
+
+def rotate(img, magnitude):
+    """rotate image by -30 to 30 based on magnitude
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image to rotate
+    magnitude : int
+        rotation angle by increments of 3 degrees
+
+    Returns
+    -------
+    tf.tensor
+        rotated image as a tensorflow tensor
+    """
+    magnitude *= 30 / 10
+    if np.random.random() > 0.5:
+        magnitude = -magnitude
+    return pil_unwrap(pil_wrap(img).rotate(magnitude))
+
+
+def sharpness(img, magnitude):
+    """increase image contrast
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int
+        sharpness magnitude between 0 and 10
+
+    Returns
+    -------
+    tf.tensor
+        transformed image
+    """
+    # rescale sharpness
+    magnitude = magnitude * 1.8 / 10 + 0.1
+    return pil_unwrap(PIL.ImageEnhance.Sharpness(pil_wrap(img)).enhance(magnitude))
+
+
+def shear(img, magnitude, axis="x"):
+    """
+    shear an image along an axis based on magnitude
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image to shear
+    magnitude : int
+        magnitude of the shear
+    axis: string, optional
+        along x or y axis, by default x
+
+    Returns
+    -------
+    tf.tensor
+        transformed image
+    """
+
+    assert axis in ["x", "y"]
+    magnitude *= 0.3 / 10
+    if np.random.random() > 0.5:
+        magnitude = -magnitude
+    img = pil_wrap(img)
+    if axis == "x":
+        # do shearX
+        return pil_unwrap(img.transform((256, 256), PIL.Image.AFFINE, (1, magnitude, 0, 0, 1, 0)))
+    else:
+        # do shearY
+        return pil_unwrap(img.transform((256, 256), PIL.Image.AFFINE, (1, 0, 0, magnitude, 1, 0)))
+
+
+def solarize(img, magnitude):
+    """applies PIL solarize to image
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int
+        magnitude of the solarize operation as an int between 0 and 10
+
+    Returns
+    -------
+    tf.tensor
+        transformed image as a tensorflow tensor
+    """
+    magnitude *= 256 / 10
+    return pil_unwrap(ImageOps.solarize(pil_wrap(img), 256 - magnitude))
+
+
+def translate(img, magnitude, axis="x"):
+    """translate image by number of pixels equal to magnitude along x or y axis
+
+    Parameters
+    ----------
+    img : tf.tensor
+        input image
+    magnitude : int
+        number of pixels to translate
+    axis : str, optional
+        axis to translate, x or y, by default "x"
+
+    Returns
+    -------
+    tf.tensor
+        translated image
+    """
+    assert isinstance(magnitude, int)
+    assert axis in ["x", "y"]
+    if np.random.random() > 0.5:
+        magnitude = -magnitude
+    img = pil_wrap(img)
+    if axis == "x":
+        # do translateX
+        return pil_unwrap(img.transform((256, 256), PIL.Image.AFFINE, (1, 0, magnitude, 0, 1, 0)))
+    else:
+        # do translateY
+        return pil_unwrap(img.transform((256, 256), PIL.Image.AFFINE, (1, 0, 0, 0, 1, magnitude)))
+
+
+class CIFAR10_policy(object):
+    """
+    Auto-augment policy for CIFAR10
+
+    Attributes
+    ----------
+    subpolicies : list of tuples
+        each element if a set of 2 operations for image augmentation representing the 25 optimal policies for CIFAR10
+
+    Methods
+    -------
+    apply_transformation:
+        apply a given transformation to an image
+    call:
+        transform an image according to a random sub-policy
+        Usage example: augmentation_policy.call(image)
+    """
+
+    def __init__(self):
+        """
+        Class Constructor
+        """
+        self.subpolicies = [
+            [(invert, 0.1, 7), (contrast, 0.2, 6)],
+            [(rotate, 0.7, 2), (translate, 0.3, 9, "x")],
+            [(sharpness, 0.8, 1), (sharpness, 0.9, 3)],
+            [(shear, 0.5, 8, "y"), (translate, 0.7, 9, "y")],
+            [(autocontrast, 0.5, 8), (equalize, 0.9, 2)],
+            [(shear, 0.2, 7, "y"), (posterize, 0.3, 7)],
+            [(color, 0.4, 3), (brightness, 0.6, 7)],
+            [(sharpness, 0.3, 9), (brightness, 0.7, 9)],
+            [(equalize, 0.6, 5), (equalize, 0.5, 1)],
+            [(contrast, 0.6, 7), (sharpness, 0.6, 5)],
+            [(color, 0.7, 7), (translate, 0.5, 8, "x")],
+            [(equalize, 0.3, 7), (autocontrast, 0.4, 8)],
+            [(translate, 0.4, 3, "y"), (sharpness, 0.2, 6)],
+            [(brightness, 0.9, 6), (color, 0.2, 8)],
+            [(solarize, 0.5, 2), (invert, 0, 3)],
+            [(equalize, 0.2, 0), (autocontrast, 0.6, 0)],
+            [(equalize, 0.2, 8), (equalize, 0.6, 4)],
+            [(color, 0.9, 9), (equalize, 0.6, 6)],
+            [(autocontrast, 0.8, 4), (solarize, 0.2, 8)],
+            [(brightness, 0.1, 3), (color, 0.7, 0)],
+            [(solarize, 0.1, 3), (autocontrast, 0.9, 3)],
+            [(translate, 0.9, 9, "y"), (translate, 0.7, 9, "y")],
+            [(autocontrast, 0.9, 2), (solarize, 0.8, 3)],
+            [(equalize, 0.8, 8), (invert, 0.1, 3)],
+            [(translate, 0.7, 9, "y"), (autocontrast, 0.9, 1)]
+        ]
+
+    def apply_transform(self, img, transformation, prob, magnitude=None, axis=None):
+        """apply a transformation to an image with probability prob
+
+        Parameters
+        ----------
+        img : tf.tensor
+            image to transform as a tensorflow tensor
+        tranformation : function
+            transformation to apply
+        prob : float
+            probability to apply the transformation
+        magnitude : int, optional
+            magnitude of the transformation, by default None
+        axis : str, optional
+            axis on which to apply transformation (translate or shear only), by default None
+
+        Returns
+        -------
+        tf tensor
+            transformed image
+        """
+        assert prob <= 1 and prob >= 0
+        if np.random.random() < prob:
+            if axis:
+                return transformation(img, magnitude, axis)
+            else:
+                return transformation(img, magnitude)
+        else:
+            return img
+
+    def call(self, image):
+        """
+        return an augmented image according to the policy
+
+        Parameters
+        ----------
+        image : tf.tensor
+            image stored as a tensor
+
+        Returns
+        -------
+        tf.tensor
+            transformed image
+        """
+        # first, choose a random sub-policy to apply
+        subpol = self.subpolicies[np.random.randint(len(self.subpolicies))]
+        for operation in subpol:
+            image = self.apply_transform(image, *operation)
+        return image
---
+++ tests/test_autoaugment.py
@@ -0,0 +1,177 @@
+import numpy as np
+import pytest
+import tensorflow as tf
+from PIL import Image
+
+from ecodse_funtime_alpha.autoaugment import autocontrast
+from ecodse_funtime_alpha.autoaugment import brightness
+from ecodse_funtime_alpha.autoaugment import CIFAR10_policy
+from ecodse_funtime_alpha.autoaugment import color
+from ecodse_funtime_alpha.autoaugment import contrast
+from ecodse_funtime_alpha.autoaugment import equalize
+from ecodse_funtime_alpha.autoaugment import invert
+from ecodse_funtime_alpha.autoaugment import pil_unwrap
+from ecodse_funtime_alpha.autoaugment import pil_wrap
+from ecodse_funtime_alpha.autoaugment import posterize
+from ecodse_funtime_alpha.autoaugment import rotate
+from ecodse_funtime_alpha.autoaugment import sharpness
+from ecodse_funtime_alpha.autoaugment import shear
+from ecodse_funtime_alpha.autoaugment import solarize
+from ecodse_funtime_alpha.autoaugment import translate
+
+tf.enable_eager_execution()
+
+
+class TestAutoAugment(object):
+    @pytest.fixture(autouse=True)
+    def mock_files(self):
+        self.img_size = 256
+        self.img_channel = 3
+        self.img_colorpixel = 128
+        self.img_color = 155
+
+        img = np.zeros((self.img_size, self.img_size, self.img_channel), dtype=np.uint8)
+        img[self.img_colorpixel:] = self.img_color
+        self.tf_img = tf.convert_to_tensor(img, dtype=tf.float32) / 255
+        self.pil_img = Image.fromarray(img)
+
+        img2 = np.zeros((self.img_size, self.img_size, self.img_channel), dtype=np.uint8)
+        img2[:, self.img_colorpixel:] = self.img_color
+        self.tf_img2 = tf.convert_to_tensor(img2, dtype=tf.float32) / 255
+
+    def test_wrap(self):
+        pil_img = pil_wrap(self.tf_img)
+        assert np.array(pil_img.getdata()).shape == (self.img_size ** 2, self.img_channel)
+        assert np.all(np.array(pil_img.getdata()).reshape(self.img_size, self.img_size, self.img_channel)[self.img_colorpixel] == self.img_color)
+
+    def test_unwrap(self):
+        tf_img = pil_unwrap(self.pil_img)
+        assert tf_img.shape == (256, 256, 3)
+        assert np.all(tf_img[self.img_colorpixel].numpy() == self.img_color / 255)
+
+    def test_autocontrast(self):
+        ac_img = autocontrast(self.tf_img)
+        assert ac_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_brightness(self):
+        magnitude = 5
+        bright_img = brightness(self.tf_img, magnitude)
+        assert bright_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_color(self):
+        magnitude = 5
+        color_img = color(self.tf_img, magnitude)
+        assert color_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_contrast(self):
+        magnitude = 5
+        contrast_img = contrast(self.tf_img, magnitude)
+        assert contrast_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_equalize(self):
+        eq_img = equalize(self.tf_img)
+        assert eq_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_invert(self):
+        invert_img = invert(self.tf_img)
+        assert np.all(abs(invert_img[self.img_colorpixel].numpy() - (1 - self.img_color / 255)) < 1e-3)
+
+    def test_posterize(self):
+        magnitude = 5
+        post_img = posterize(self.tf_img, magnitude)
+        assert post_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_rotate(self):
+        magnitude = 5
+        # np.random.random() > 0.5
+        np.random.seed(0)
+        rotate_img = rotate(self.tf_img, magnitude)
+        assert np.all(rotate_img[self.img_colorpixel, 0].numpy() == self.img_color / 255)
+        assert np.all(rotate_img[self.img_colorpixel, -1].numpy() == 0)
+        # np.random.random() < 0.5
+        np.random.seed(1)
+        rotate_img = rotate(self.tf_img, magnitude)
+        assert np.all(rotate_img[self.img_colorpixel, 0].numpy() == 0)
+        assert np.all(rotate_img[self.img_colorpixel, -1].numpy() == self.img_color / 255)
+
+    def test_sharpness(self):
+        magnitude = 5
+        sharp_img = sharpness(self.tf_img, magnitude)
+        assert sharp_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_shearX(self):
+        shear_magnitude = 4
+        # np.random.random() > 0.5
+        np.random.seed(0)
+        shear_img = shear(self.tf_img, shear_magnitude, axis="x")
+        assert np.all(shear_img[self.img_colorpixel, 0].numpy() == 0)
+        assert np.all(shear_img[self.img_colorpixel, -1].numpy() == self.img_color / 255)
+        # np.random.random() < 0.5
+        np.random.seed(1)
+        shear_img = shear(self.tf_img, shear_magnitude, axis="x")
+        assert np.all(shear_img[self.img_colorpixel, 0].numpy() == self.img_color / 255)
+        assert np.all(shear_img[self.img_colorpixel, -1].numpy() == 0)
+
+    def test_shearY(self):
+        shear_magnitude = 4
+        # np.random.random() > 0.5
+        np.random.seed(0)
+        shear_img = shear(self.tf_img2, shear_magnitude, axis="y")
+        assert np.all(shear_img[0, self.img_colorpixel].numpy() == 0)
+        assert np.all(shear_img[-1, self.img_colorpixel].numpy() == self.img_color / 255)
+        # np.random.random() < 0.5
+        np.random.seed(1)
+        shear_img = shear(self.tf_img2, shear_magnitude, axis="y")
+        assert np.all(shear_img[0, self.img_colorpixel].numpy() == self.img_color / 255)
+        assert np.all(shear_img[-1, self.img_colorpixel].numpy() == 0)
+
+    def test_solarize(self):
+        magnitude = 5
+        sol_img = solarize(self.tf_img, magnitude)
+        assert sol_img.shape == (self.img_size, self.img_size, self.img_channel)
+
+    def test_translateX(self):
+        magnitude = 5
+        # np.random.random() > 0.5
+        np.random.seed(0)
+        translate_img = translate(self.tf_img2, magnitude, axis="x")
+        assert np.all(translate_img[:, magnitude:self.img_colorpixel + magnitude].numpy() == 0)
+        assert np.all(translate_img[:, self.img_colorpixel + magnitude:].numpy() == self.img_color / 255)
+        # np.random.random() < 0.5
+        np.random.seed(1)
+        translate_img = translate(self.tf_img2, magnitude, axis="x")
+        assert np.all(translate_img[:, :self.img_colorpixel - magnitude].numpy() == 0)
+        assert np.all(translate_img[:, self.img_colorpixel - magnitude:-magnitude].numpy() == self.img_color / 255)
+        assert np.all(translate_img[:, -magnitude:].numpy() == 0)
+
+    def test_translateY(self):
+        magnitude = 5
+        # np.random.random() > 0.5
+        np.random.seed(0)
+        translate_img = translate(self.tf_img, magnitude, axis="y")
+        assert np.all(translate_img[magnitude:self.img_colorpixel + magnitude].numpy() == 0)
+        assert np.all(translate_img[self.img_colorpixel + magnitude:].numpy() == self.img_color / 255)
+        # np.random.random() < 0.5
+        np.random.seed(1)
+        translate_img = translate(self.tf_img, magnitude, axis="y")
+        assert np.all(translate_img[:self.img_colorpixel - magnitude].numpy() == 0)
+        assert np.all(translate_img[self.img_colorpixel - magnitude:-magnitude].numpy() == self.img_color / 255)
+        assert np.all(translate_img[-magnitude:].numpy() == 0)
+
+    def test_cifar10_applytransform(self):
+        augment_policy = CIFAR10_policy()
+        magnitude = 8
+        autocontrast_img = augment_policy.apply_transform(self.tf_img, autocontrast, 1, magnitude)
+        assert np.all(autocontrast_img.numpy() == autocontrast(self.tf_img, magnitude).numpy())
+        id_img = augment_policy.apply_transform(self.tf_img, autocontrast, 0, magnitude)
+        assert np.all(id_img == self.tf_img)
+        np.random.seed(0)
+        shear_img = augment_policy.apply_transform(self.tf_img, shear, 1, magnitude, "x")
+        np.random.seed(0)
+        assert np.all(abs(shear_img.numpy() - shear(self.tf_img, magnitude, "x")) < 1e-3)
+
+    def test_cifar10_call(self):
+        augment_policy = CIFAR10_policy()
+        np.random.seed(0)
+        aug_img = augment_policy.call(self.tf_img)
+        assert aug_img.shape == self.tf_img.shape
